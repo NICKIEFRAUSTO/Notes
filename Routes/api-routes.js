ed3968b5c434de2a8ddfabbc1d60c30b52cec7c8
Nickie Frausto <79732055+NICKIEFRAUSTO@users.noreply.github.com>
2021-04-15 23:00:39 -0500

    added delete not function, add note function and save note function

--- Routes/api-routes.js
+++ Routes/api-routes.js
@@ -2,7 +2,10 @@
 // * `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into `npm` packages that could do this for you).
 // const { v1: uuidv1 } = require("uuid");
 const path = require("path");
-const notesArray = require("../db/db.json");
+let notesArray = require("../db/db.json");
+const fs = require("fs");
+
+let uniqid = require("uniqid");
 
 module.exports = (app) => {
   app.get("/api/notes", (req, res) =>
@@ -12,18 +15,36 @@ module.exports = (app) => {
   app.post("/api/notes", (req, res) => {
     // uuidv4();
     const saveNote = req.body;
+    let newID = uniqid();
     let newNote = {
       title: saveNote.title,
       text: saveNote.text,
-      id: "1",
+      id: newID,
     };
+
     console.log(saveNote);
+    notesArray.push(newNote);
+
+    writeToFile();
+    res.json(notesArray);
     // res.sendFile(path.join(__dirname, "../db/db.json"));
   });
 
   app.delete("/api/notes/:id", (req, res) => {
-    // const deleteNoteId = req.params.id;
-    // console.log(id);
-    // filter((note) => note.id != deleteNoteId);
+    let deleteNoteId = req.params.id;
+    console.log(deleteNoteId);
+    notesArray = notesArray.filter((note) => note.id != deleteNoteId);
+
+    writeToFile();
+    res.json(notesArray);
   });
 };
+
+function writeToFile() {
+  const fileName = "./db/db.json";
+  let fileString = JSON.stringify(notesArray);
+
+  fs.writeFile(fileName, fileString, (err) =>
+    err ? console.log(err) : console.log("Updating db.json...")
+  );
+}
